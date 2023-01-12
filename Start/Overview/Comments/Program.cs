199d601bc89c5818d4287a6492306737e6ec1a4d
carloscmedina77 <carloscmedina@yahoo.com>
2023-01-12 16:41:45 -0600

    Added Files

---
+++ Start/Overview/Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //single line comments start with two slashes and you can have as many as you want
+            /* multiple line comment start with a slash and a start
+            you can just keep on going until you close it with 
+            another */
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
---
+++ Start/Program Flow/Exceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1002;
+            int y =0;
+            int result;
+
+            // TODO: try-catch expressions make error checking easier
+            try{
+                if (x > 1000){
+                    throw new ArgumentOutOfRangeException("x", "x has to be 1000 or less");
+                }
+                result = x / y;
+                Console.WriteLine("The result is: {0}", result);
+            }
+            catch (DivideByZeroException e ){
+                Console.WriteLine("Nah man, nah!");
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentOutOfRangeException e) {
+                Console.WriteLine("Sorry, 1000 it dude!");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                Console.WriteLine("This code always runs");
+            }
+        }
+    }
+}
